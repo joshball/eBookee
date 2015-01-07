176c5400e082892c4fa7335d42be5360ca9b8f92
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-07 08:17:30 -0800

    Adding EBook class

---
+++ lib/eBookee/ContentSection.cs
@@ -0,0 +1,11 @@
+namespace eBookee
+{
+    public class ContentSection
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string RawContent { get; set; }
+        public string RawContentType { get; set; }
+        public string AsText { get; set; }
+    }
+}
---
+++ lib/eBookee/EBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookee
+{
+    public class EBook
+    {
+        public EBook(string ebookFile)
+        {
+            this.ContentSections = new List<ContentSection>();
+        }
+
+        public IEnumerable<ContentSection> ContentSections { get; set; }
+        public string Title { get; set; }
+    }
+}
